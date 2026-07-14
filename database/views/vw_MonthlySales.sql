72a93f1f5698ffe8fcd06661b999a55970167471
Kavindu Gayantha <kavindugayantha178@gmail.com>
2026-07-14 23:35:50 +0530

    fix issue

--- database/views/vw_MonthlySales.sql
+++ database/views/vw_MonthlySales.sql
@@ -1,23 +1,30 @@
 USE SalesAnalytics_DB;
 GO
 
-CREATE VIEW vw_MonthlySales AS
+CREATE OR ALTER VIEW vw_MonthlySales AS
 
 SELECT
 
     YEAR(order_purchase_timestamp) AS sales_year,
-    MONTH(order_purchase_timestamp) AS sales_month,
+
+    MONTH(order_purchase_timestamp) AS month_number,
+
+    DATENAME(MONTH, order_purchase_timestamp) AS month_name,
+
+    FORMAT(order_purchase_timestamp, 'yyyy-MM') AS year_month,
 
     COUNT(order_id) AS total_orders,
 
-    SUM(total_order_value) AS total_revenue,
+    ROUND(SUM(total_order_value), 2) AS total_revenue,
 
-    AVG(total_order_value) AS average_order_value
+    ROUND(AVG(total_order_value), 2) AS average_order_value
 
 FROM vw_SalesSummary
 
 GROUP BY
 
     YEAR(order_purchase_timestamp),
-    MONTH(order_purchase_timestamp);
+    MONTH(order_purchase_timestamp),
+    DATENAME(MONTH, order_purchase_timestamp),
+    FORMAT(order_purchase_timestamp, 'yyyy-MM');
 GO
